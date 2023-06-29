d905e1bca9b25dee38b43e384668998f8776deff
Zarina Kate Lagatic <zarlagatic@my.cspc.edu.ph>
2023-06-29 11:55:39 +0800

    Create Admin Dashboard

--- AppointEase/resources/views/index.blade.php
+++ AppointEase/resources/views/index.blade.php
@@ -1,3 +1,62 @@
-<x-layout >
+<x-layout>
     <x-sidebar />
-</x-layout>
+
+    <style>
+        .glass-card {
+            background: rgba(255, 255, 255, 0.2);
+            box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+            backdrop-filter: blur(8.5px);
+            -webkit-backdrop-filter: blur(8.5px);
+            border-radius: 10px;
+            padding: 20px;
+            color: white;
+        }
+    </style>
+    <section class="section dashboard">
+      <div class="row">
+        <div class="col-xxl-4 col-md-6">
+          <div class="card glass-card">
+            <div class="card-body">
+              <h5 class="card-title text-white">Today's Total Appointment</h5>
+              <div class="d-flex align-items-center">
+                <div class="ps-3">
+                  <h6>145</h6>
+                  <span class="text-success small pt-1 fw-bold">12%</span>
+                  <span class="text-white">increase</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-4 col-md-6">
+          <div class="card glass-card">
+            <div class="card-body">
+              <h5 class="card-title text-white">Weekly Total Appointment</span></h5>
+              <div class="d-flex align-items-center">
+                <div class="ps-3">
+                  <h6>$3,264</h6>
+                  <span class="text-success small pt-1 fw-bold">8%</span>
+                  <span class="text-white">increase</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xxl-4 col-xl-12">
+          <div class="card glass-card">
+            <div class="card-body">
+              <h5 class="card-title text-white">Total Number of Clients</span></h5>
+              <div class="d-flex align-items-center">
+                <div class="ps-3">
+                  <h6>1244</h6>
+                  <span class="text-danger small pt-1 fw-bold">12%</span>
+                  <span class="text-white">decrease</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </x-layout>
+  
--- AppointEase/routes/web.php
+++ AppointEase/routes/web.php
@@ -21,3 +21,8 @@ Route::get('/login', function () {
     return view('login');
 });
 
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+
