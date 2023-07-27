3e8bbb01675bec147bc0514f2a0600c004b75230
Ivan Jefferson O. Calleja <biboy.calleja0872@gmail.com>
2023-07-27 22:33:13 +0800

    Update SocialAuthController & Users Table

--- AppointEase/app/Http/Controllers/SocialAuthController.php
+++ AppointEase/app/Http/Controllers/SocialAuthController.php
@@ -1,12 +1,13 @@
 <?php
+
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Log;
-use Illuminate\Http\Request;
-use Illuminate\Routing\Controller;
-use Laravel\Socialite\Facades\Socialite;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\DB;
+
 
 class SocialAuthController extends Controller
 {
@@ -17,47 +18,30 @@ class SocialAuthController extends Controller
 
     public function handleProviderCallback()
     {
-        Log::info('handleProviderCallback - Start');
+        try {
+            $google_user = Socialite::driver('google')->user();
+            $user = User::where('email', $google_user->getEmail())->first();
 
-        $user = Socialite::driver('google')->user();
+            if (!$user) {
+                // User does not exist in the database
+                $new_user = User::create([
+                    'name' => $google_user->getName(),
+                    'email' => $google_user->getEmail(),
+                    'google_id' => $google_user->getId(),
+                ]);
 
-        //dd($user);
+                Auth::login($new_user);
 
-        Log::info('User Data:', (array)$user);
+                return redirect()->intended('home');
+            } else {
+                Auth::login($user);
 
-        $allowedDomain = 'my.cspc.edu.ph';
-        $userEmailDomain = explode('@my.cspc.edu.ph', $user->email)[1];
-
-        Log::info('User Email Domain:', ['email_domain' => $userEmailDomain]);
-
-        if ($userEmailDomain !== $allowedDomain) {
-            Log::warning('Unauthorized access attempt from domain: ' . $userEmailDomain);
-
-            return redirect('/login')->with('error', 'You are not authorized to access this application.');
+                return redirect()->intended('home');
+            }
+        } catch (\Throwable $th) {
+            dd('Something went wrong!' . $th->getMessage());
         }
-
-        $existingUser = User::where('email', $user->getEmail())->first();
-
-        if (!$existingUser) {
-            Log::info('Creating a new user');
-
-            $newUser = User::create([
-                'name' => $user->getName(),
-                'email' => $user->getEmail(),
-                'google_id' => $user->getId()
-            ]);
-
-            Log::info('New User Data:', ['user' => $newUser->toArray()]);
-
-            Auth::guard()->login($newUser);
-        } else {
-            Log::info('Existing user login');
-
-            Auth::guard()->login($existingUser);
-        }
-
-        Log::info('handleProviderCallback - End');
-
-        return redirect('/home');
     }
+
+    // ...
 }
